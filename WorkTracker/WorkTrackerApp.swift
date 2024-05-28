1d37b3e62127bce485e5e2a58c8a3293dad7eef5
Hayden Steele <haydenallensteene@gmail.com>
2024-05-28 15:35:27 -0600

    Initial Commit

---
+++ WorkTracker/WorkTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkTrackerApp.swift
+//  WorkTracker
+//
+//  Created by Hayden Steele on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
